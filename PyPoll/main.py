6def2c0c4eaa90e35e05a27df23e9c96a5df2933
Andrew Scott <andrewscott@Andrews-MacBook-Pro.local>
2019-03-15 19:43:17 -0700

    Completed

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -21,9 +21,25 @@ sumvotes = election_df["Voter ID"].count()
 #--Find winner
 winner = election_df.groupby('Candidate')['Voter ID'].count().idxmax()
 
-#--Calculate other winners
+avg = election_df.groupby(['Candidate']).count()
+#avg.head(10)
 
+#--Getting percentage of votes into dataframe
+avg["Percent of Votes"] = avg["Voter ID"] / sumvotes * 100
 
+#avg
+
+#--Reorganizing my columns to fit output
+output = avg[["Percent of Votes","Voter ID"]]
+
+#output.head()
+#renaming my columns
+renamed_df = output.rename(columns={"Percent of Votes":"Percent of Votes", "Voter ID" : "Sum of Votes"})
+
+#sorting by Sum of Votes
+final_output = renamed_df.sort_values("Sum of Votes", ascending=False)
+
+#final_output.head()
 
 
 #--Output to Screen
@@ -32,9 +48,35 @@ print("Election Results")
 print("---------------------------")
 print(f"Total Votes: ", + sumvotes)
 print("---------------------------")
-
-
-
+print(final_output)
 print("---------------------------")
 print(f"Winner: " + str(winner))
 print("---------------------------")
+
+#--------------------------------------------------------------------
+#--Let's write this to an output file
+#--------------------------------------------------------------------
+
+text_file = open("PyPoll Results.txt", "w")
+text_file.write("") 
+text_file.write("\n")
+text_file.write("Election Results")
+text_file.write("\n")
+text_file.write("-------------------------")
+text_file.write("\n")
+text_file.write("Total Votes: ")
+text_file.write(str(sumvotes))
+text_file.write("\n")
+text_file.write("-------------------------")
+text_file.write("\n")
+text_file.write(str(final_output))
+text_file.write("\n")
+text_file.write("-------------------------")
+text_file.write("\n")
+text_file.write("Winner: ")
+text_file.write(str(winner))
+text_file.write("\n")
+text_file.write("-------------------------")
+text_file.write("\n")
+text_file.close()
+#--End
